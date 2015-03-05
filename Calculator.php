ef02a51dc60241700c92badc9d1fa431004fb940
David Bedford <beds2005@gmail.com>
2015-03-05 19:46:04 +0000

    Instaniating the Calculator class

--- Calculator.php
+++ Calculator.php
@@ -4,9 +4,18 @@
 * Simple calculator class
 */
 
-class Calculator {
-	public function __construct()
-	{
+class Calculator extends Operations {
+	private $input;
 
+	/**
+	* Constructor method for Calculator
+	*
+	* @param string $input equation to solve
+	*/
+	public function __construct($input)
+	{
+		$this->input = $input;
 	}	
 }
+
+$calculator = new Calculator("1 + 1 * 3 + 3");
