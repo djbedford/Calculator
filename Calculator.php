2087013b854acd30e7b41e1b5256b98e2c3c43ba
David Bedford <beds2005@gmail.com>
2015-03-05 20:28:39 +0000

    Fleshing out solveEquation

--- Calculator.php
+++ Calculator.php
@@ -34,9 +34,21 @@ class Calculator extends Operations {
 
 	private function solveEquation($equation)
 	{
-		if (preg_match('/(.+)\s(([0-9]+)\s[*]\s([0-9]+))\s(.+)/', $equation, $matches)) {
+		if (preg_match('/([0-9.*-/+\s]*)(([0-9]+)\s[*]\s([0-9]+))([0-9.*-/+\s]*)/', $equation, $matches)) {
 			$multiply = $this->multiply($matches[3], $matches[4]);
+			$equation = $matches[1] . ' ' . $multiply . ' ' . $matches[5];
+		} elseif (preg_match('/([0-9.*-/+\s]*)(([0-9]+)\s[/]\s([0-9]+))([0-9.*-/+\s]*)/', $equation, $matches)) {
+			$division = $this->divide($matches[3], $matches[4]);
+			$equation = $matches[1] . ' ' . $division . ' ' . $matches[5];
+		} elseif (preg_match('/([0-9.*-/+\s]*)(([0-9]+)\s[+]\s([0-9]+))([0-9.*-/+\s]*)/', $equation, $matches)) {
+			$addition = $this->add($matches[3], $matches[4]);
+			$equation = $matches[1] . ' ' . $addition . ' ' . $matches[5];
+		} elseif (preg_match('/([0-9.*-/+\s]*)(([0-9]+)\s[-]\s([0-9]+))([0-9.*-/+\s]*)/', $equation, $matches)) {
+			$subtraction = $this->subtract($matches[3], $matches[4]);
+			$equation = $matches[1] . ' ' . $subtraction . ' ' . $matches[5];
 		}
+
+		return $this->solveEquation($equation);
 	}
 }
 
