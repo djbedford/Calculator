b6751f609d2aa1abcc26c6294cbd14dc358bde3e
David Bedford <beds2005@gmail.com>
2015-03-05 19:55:36 +0000

    Formatting input

--- Calculator.php
+++ Calculator.php
@@ -15,7 +15,27 @@ class Calculator extends Operations {
 	public function __construct($input)
 	{
 		$this->input = $input;
-	}	
+
+		$input = $this->formatInput($input);
+		
+		$this->solveEquation($input);
+	}
+
+	/**
+	* Remove the white space from the input
+	*
+	* @param string $input input string
+	* @return string formatted string with whitespace removed
+	*/
+	private function formatInput($input)
+	{
+		return str_replace(' ', '', $input);
+	}
+
+	private function solveEquation($equation)
+	{
+
+	}
 }
 
 $calculator = new Calculator("1 + 1 * 3 + 3");
