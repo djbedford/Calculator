21106fb50bbeab25851c413b5274db96d4d900a9
David Bedford <beds2005@gmail.com>
2015-03-05 19:36:38 +0000

    Initial setup of operations

---
+++ Operations.php
@@ -0,0 +1,54 @@
+<?php
+/**
+* Contains basic operations for the calculator
+*/
+abstract class Operations {
+
+	/**
+	* Adds two operands
+	*
+	* @param string $operand1 first number
+	* @param string $operand2 second number
+	* @return number result of adding the first and second operand together
+	*/
+	public function add($operand1, $operand2)
+	{
+
+	}
+
+	/**
+	* Subtracts two operands
+	*
+	* @param string $operand1 first number
+	* @param string $operand2 second number
+	* @return number result of subtracting the first and second operand together
+	*/
+	public function subtract($operand1, $operand2)
+	{
+
+	}
+
+	/**
+	* Multiples two operands
+	*
+	* @param string $operand1 first number
+	* @param string $operand2 second number
+	* @return number result of multiplying the first and second operand together
+	*/
+	public function multiply($operand1, $operand2)
+	{
+
+	}
+
+	/**
+	* Divides two operands
+	*
+	* @param string $operand1 first number
+	* @param string $operand2 second number
+	* @return number result of dividing the first and second operand together
+	*/
+	public function divide($operand1, $operand2)
+	{
+
+	}
+}
